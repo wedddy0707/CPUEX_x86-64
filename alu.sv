ff4fb17a3e7e992cbdf950e097fc1c2f2add7acb
wedddy0707 <ryoryo.ueda@gmail.com>
2020-03-02 16:09:04 +0900

    SUB, AND, ORの追加

--- alu.sv
+++ alu.sv
@@ -10,6 +10,8 @@ module alu (
 );
   wire [`REG_W-1:0] add_d;
   wire [`REG_W-1:0] sll_d;
+  wire [`REG_W-1:0] and_d;
+  wire [`REG_W-1:0]  or_d;
   wire [`REG_W-1:0] xor_d;
   wire [`REG_W-1:0] mov_d;
   wire [`REG_W-1:0] lea_d;
@@ -17,8 +19,15 @@ module alu (
   assign d =
     (opcode==`MICRO_ADD ) ? add_d :
     (opcode==`MICRO_ADDI) ? add_d :
+    (opcode==`MICRO_SUB ) ? sub_d :
+    (opcode==`MICRO_SUBI) ? sub_d :
     (opcode==`MICRO_SLLI) ? sll_d :
+    (opcode==`MICRO_AND ) ? and_d :
+    (opcode==`MICRO_ANDI) ? and_d :
+    (opcode==`MICRO_OR  ) ?  or_d :
+    (opcode==`MICRO_ORI ) ?  or_d :
     (opcode==`MICRO_XOR ) ? xor_d :
+    (opcode==`MICRO_XORI) ? xor_d :
     (opcode==`MICRO_MOV ) ? mov_d :
     (opcode==`MICRO_MOVI) ? mov_d :
     (opcode==`MICRO_LEA ) ? lea_d : 0;
@@ -29,16 +38,34 @@ module alu (
     .t        (`MICRO_ADDI ? imm:t),
     .bit_mode (bit_mode)
   );
+  execute_aub sub_1 (
+    .d        (sub_d),
+    .s        (s),
+    .t        (`MICRO_SUBI ? imm:t),
+    .bit_mode (bit_mode)
+  );
   execute_sll sll_1 (
     .d        (sll_d),
     .s        (s),
     .t        (`MICRO_SLLI ? imm:t),
     .bit_mode (bit_mode)
   );
+  execute_and and_1 (
+    .d        (and_d),
+    .s        (s),
+    .t        (`MICRO_ANDI ? imm:t),
+    .bit_mode (bit_mode)
+  );
+  execute_or or_1 (
+    .d        (or_d),
+    .s        (s),
+    .t        (`MICRO_ORI  ? imm:t),
+    .bit_mode (bit_mode)
+  );
   execute_xor xor_1 (
     .d        (xor_d),
     .s        (s),
-    .t        (t),
+    .t        (`MICRO_XORI ? imm:t),
     .bit_mode (bit_mode)
   );
   execute_mov mov_1 (
@@ -105,6 +132,40 @@ module execute_sub (
     .t        (~t+1),
     .bit_mode (bit_mode)
   );
+endmodule
+
+module execute_and (
+  output reg [`REG_W     -1:0] d,
+  input wire [`REG_W     -1:0] s,
+  input wire [`REG_W     -1:0] t,
+  input wire [`BIT_MODE_W-1:0] bit_mode
+);
+  wire [ 7:0] d_08bit =  8'(s) &  8'(t);
+  wire [15:0] d_16bit = 16'(s) & 16'(t);
+  wire [31:0] d_32bit = 32'(s) & 32'(t);
+  wire [63:0] d_64bit = 64'(s) & 64'(t);
+
+  assign d =
+    (bit_mode==`BIT_MODE_8 ) ? `REG_W'(d_08bit) :
+    (bit_mode==`BIT_MODE_16) ? `REG_W'(d_16bit) :
+    (bit_mode==`BIT_MODE_32) ? `REG_W'(d_32bit) : `REG_W'(d_64bit);
+endmodule
+
+module execute_or (
+  output reg [`REG_W     -1:0] d,
+  input wire [`REG_W     -1:0] s,
+  input wire [`REG_W     -1:0] t,
+  input wire [`BIT_MODE_W-1:0] bit_mode
+);
+  wire [ 7:0] d_08bit =  8'(s) |  8'(t);
+  wire [15:0] d_16bit = 16'(s) | 16'(t);
+  wire [31:0] d_32bit = 32'(s) | 32'(t);
+  wire [63:0] d_64bit = 64'(s) | 64'(t);
+
+  assign d =
+    (bit_mode==`BIT_MODE_8 ) ? `REG_W'(d_08bit) :
+    (bit_mode==`BIT_MODE_16) ? `REG_W'(d_16bit) :
+    (bit_mode==`BIT_MODE_32) ? `REG_W'(d_32bit) : `REG_W'(d_64bit);
 endmodule
 
 module execute_xor (
