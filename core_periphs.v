3f7da8a11a82ceac5f074e0c3ba05393796855aa
wedddy0707 <ryoryo.ueda@gmail.com>
2020-03-03 13:06:04 +0900

    typoしてた

--- core_periphs.v
+++ core_periphs.v
@@ -2,8 +2,10 @@
 `include "common_params.h"
 
 module core_periphs #(
-  parameter LOAD_LATEMCY  =  1,
-  parameter ACTUAL_ADDR_W = 13
+  parameter LOAD_LATEMCY  =    1,
+  parameter ACTUAL_ADDR_W =   13,
+  parameter INIT_RIP      =    0,
+  parameter INIT_RSP      = 1024
 ) (
   input  wire [`DATA_W      -1:0] ld_data_for_inst,
   input  wire [`DATA_W      -1:0] ld_data         ,
@@ -20,7 +22,9 @@ module core_periphs #(
   assign mem_addr  =  mem_addr_with_ideal_width[ACTUAL_ADDR_W-1:0];
   assign pc_to_mem = pc_to_mem_with_ideal_width[ACTUAL_ADDR_W-1:0];
   core #(
-    LOAD_LATEMCY
+    LOAD_LATEMCY,
+    INIT_RIP    ,
+    INIT_RSP
   ) core_1 (
     .ld_data_for_inst(ld_data_for_inst          ),
     .ld_data         (ld_data                   ),
